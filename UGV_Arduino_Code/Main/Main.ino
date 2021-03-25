d4a266cb8fb5b1ee9dbb180e9b75f02b6e788af2
PikaSannnnn <Dong.Stephen11@gmail.com>
2021-03-25 14:22:04 -0700

    Updated Altimeter

--- UGV_Arduino_Code/Main/Main.ino
+++ UGV_Arduino_Code/Main/Main.ino
@@ -8,10 +8,35 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //  switch(state){
+  //   case 0:
+  //   {
+  //     //flying state
+  //     isFlying = detectIfAboveAlt(MIN_FLIGHT_LEVEL);
+      
+  //     if(!isFlying)
+  //     {
+  //       Serial.println("\nState 1");
+  //       state = 1;
+  //     }
+  //     Serial.println("\nStill state 0");
+  //     delay(500);
+  //     break;
+  //   }
+  //   case 1:
+  //   {
+  //     Serial.println("\nWe are here now");
+  //     //dropping state
+  //     if(isGrounded)
+  //     {
+  //       state = 2;
+  //     }
+  //     delay(500);
+  //     break;
+  //   }
+  //  }
 }
 
-void orientFix() {
+// void orientFix() {
   
-}
+// }
